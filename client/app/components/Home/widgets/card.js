6cbffbc96b8a70d0f10eb4d47dd81035906c2cf8
Oluwafemi Olasubomi <olasubomifemi98@gmail.com>
2020-05-09 05:27:40 -0700

    optimization -2

--- client/app/components/Home/widgets/card.js
+++ client/app/components/Home/widgets/card.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import img from '../../../../public/assets/img/iphone_11_pro_max.png';
+import img from '../../../../public/assets/img/5S.png';
 
 
 function CardComponent({ data, action }) {
@@ -8,7 +8,7 @@ function CardComponent({ data, action }) {
   return (
     <div className='card'>
       <div className='condition'>
-        C/D
+       <span>C/D</span> 
        </div>
       <div className='img'>
         <img src={img} />
@@ -20,6 +20,11 @@ function CardComponent({ data, action }) {
         <h3>$450</h3>
         <p>1500 available</p>
 
+      </div>
+      <div className='button'>
+        <button  className='btn btn-primary'>
+          Buy
+        </button>
       </div>
     </div>
   );
