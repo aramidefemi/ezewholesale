c9b9d2651ce36c52b1e01eee00d9e8f3e1b9f033
Oluwafemi Olasubomi <olasubomifemi98@gmail.com>
2020-05-09 05:36:32 -0700

    optimization -3

--- client/app/components/Home/widgets/card.js
+++ client/app/components/Home/widgets/card.js
@@ -16,13 +16,13 @@ function CardComponent({ data, action }) {
       <div className='description'>
         <h4>iPhone 8 Plus</h4>
         <p>UNLOCKED | 265GB</p>
-        <p><small>unit price</small></p>
+        <p><small>Unit price</small></p>
         <h3>$450</h3>
-        <p>1500 available</p>
+        <p>1500 Available</p>
 
       </div>
-      <div className='button'>
-        <button  className='btn btn-primary'>
+      <div className='button mt-4 px-4'>
+        <button  className='btn btn-primary btn-block'>
           Buy
         </button>
       </div>
